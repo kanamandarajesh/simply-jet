3f4c9927fbe91e849fa0f4d406158a089788baf6
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-16 14:53:18 +0530

    Create iam_roles.tf

---
+++ iam_roles.tf
@@ -0,0 +1,83 @@
+# IAM role for EKS worker nodes to assume
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach the Amazon EKS worker node policy
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+# Attach the Amazon EC2 container registry policy to the worker node role
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Attach autoscaling permissions
+resource "aws_iam_role_policy_attachment" "eks_autoscaler_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
+}
+
+# IAM role for S3 access (e.g., for storing database backups)
+resource "aws_iam_role" "s3_access_role" {
+  name = "s3-access-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach S3 full access policy to the role
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
+  role       = aws_iam_role.s3_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# IRSA (IAM Roles for Service Accounts) - EKS pods access to S3
+resource "aws_iam_policy" "s3_policy" {
+  name        = "s3-access-policy"
+  
+  description = "Policy to allow EKS pods to access S3"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "s3:ListBucket",
+        "s3:GetObject",
+        "s3:PutObject"
+      ],
+      Resource = [
+        "arn:aws:s3:::${var.s3_bucket_name}",
+        "arn:aws:s3:::${var.s3_bucket_name}/*"
+      ]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
+  role       = aws_iam_role.s3_access_role.name
+  policy_arn = aws_iam_policy.s3_policy.arn
+}
