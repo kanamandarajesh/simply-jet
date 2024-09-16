7a4a8e0e565da3e3c9c1569d1cd93a74e7dd0247
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-16 14:36:58 +0530

    Create security_groups.tf

---
+++ security_groups.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "app_sg" {
+  vpc_id = aws_vpc.main.id
+  
+  description = "Allow HTTP and HTTPS traffic"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "app-security-group"
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  vpc_id = aws_vpc.main.id
+  
+  description = "Allow PostgreSQL and Redis traffic"
+  
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "db-security-group"
+  }
+}
