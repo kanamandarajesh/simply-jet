ac74e33ee62f7815b23d6b71fbd8f391a5da33e6
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-16 14:41:09 +0530

    Create route53_variables.tf

---
+++ route53_variables.tf
@@ -0,0 +1,9 @@
+variable "domain_name" {
+  description = "Domain name for Route 53"
+  default     = "example.com"
+}
+
+variable "app_domain_name" {
+  description = "Application domain name"
+  default     = "app.example.com"
+}
